ef6cad063ec53c7c1061f73fc09e1cbb9bfc81e4
Msstt <1064263053@qq.com>
2025-02-11 22:32:40 +0800

    poll chat server

--- Assets/Scripts/Echo.cs
+++ Assets/Scripts/Echo.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Tracing;
 using System.Net.Sockets;
 using TMPro;
 using UnityEngine;
@@ -12,48 +14,26 @@ public class Echo : MonoBehaviour {
 
   private Socket socket;
   private readonly byte[] buffer = new byte[1024];
-  private string message = string.Empty;
 
   private void Update() {
-    text.text = message;
+    if (socket == null) {
+      return;
+    }
+    if (socket.Poll(0, SelectMode.SelectRead)) {
+      int bytesCount = socket.Receive(buffer);
+      Debug.Log("S" + bytesCount);
+      text.text = text.text + "\n" + System.Text.Encoding.UTF8.GetString(buffer, 0, bytesCount);
+    }
   }
 
   public void Connection() {
     socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-    socket.BeginConnect("127.0.0.1", 8888, OnConnect, null);
-  }
-
-  private void OnConnect(IAsyncResult ar) {
-    try {
-      socket.EndConnect(ar);
-      socket.BeginReceive(buffer, 0, 1024, 0, OnReceive, null);
-      Debug.Log("Socket connection success.");
-    } catch (SocketException ex) {
-      Debug.Log("Socket connection failed!" + ex);
-    }
-  }
-
-  private void OnReceive(IAsyncResult ar) {
-    try {
-      int bytesCount = socket.EndReceive(ar);
-      message += "\n" + System.Text.Encoding.UTF8.GetString(buffer, 0, bytesCount);
-      socket.BeginReceive(buffer, 0, 1024, 0, OnReceive, null);
-    } catch (SocketException ex) {
-      Debug.Log("Socket receive failed!" + ex);
-    }
+    socket.Connect("127.0.0.1", 8888);
+    Debug.Log("Socket connect success.");
   }
 
   public void Send() {
     byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(inputField.text);
-    socket.BeginSend(sendBytes, 0, sendBytes.Length, 0, OnSend, null);
-  }
-
-  private void OnSend(IAsyncResult ar) {
-    try {
-      int bytesCount = socket.EndSend(ar);
-      Debug.Log("Socket send " + bytesCount + " bytes success.");
-    } catch (SocketException ex) {
-      Debug.Log("Socket send failed!" + ex);
-    }
+    socket.Send(sendBytes);
   }
 }
