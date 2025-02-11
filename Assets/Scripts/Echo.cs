e5e43a3d4112bfefbabbc749d364e9b4d2ae0429
Msstt <1064263053@qq.com>
2025-02-11 21:54:13 +0800

    async chat server

--- Assets/Scripts/Echo.cs
+++ Assets/Scripts/Echo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
@@ -10,19 +11,49 @@ public class Echo : MonoBehaviour {
   public TMP_Text text;
 
   private Socket socket;
+  private readonly byte[] buffer = new byte[1024];
+  private string message = string.Empty;
+
+  private void Update() {
+    text.text = message;
+  }
 
   public void Connection() {
     socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-    socket.Connect("127.0.0.1", 8888);
-    Debug.Log("Socket connection success.");
+    socket.BeginConnect("127.0.0.1", 8888, OnConnect, null);
+  }
+
+  private void OnConnect(IAsyncResult ar) {
+    try {
+      socket.EndConnect(ar);
+      socket.BeginReceive(buffer, 0, 1024, 0, OnReceive, null);
+      Debug.Log("Socket connection success.");
+    } catch (SocketException ex) {
+      Debug.Log("Socket connection failed!" + ex);
+    }
+  }
+
+  private void OnReceive(IAsyncResult ar) {
+    try {
+      int bytesCount = socket.EndReceive(ar);
+      message += "\n" + System.Text.Encoding.UTF8.GetString(buffer, 0, bytesCount);
+      socket.BeginReceive(buffer, 0, 1024, 0, OnReceive, null);
+    } catch (SocketException ex) {
+      Debug.Log("Socket receive failed!" + ex);
+    }
   }
 
   public void Send() {
     byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(inputField.text);
-    socket.Send(sendBytes);
-    byte[] buffer = new byte[1024];
-    int bytesCount = socket.Receive(buffer);
-    text.text = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesCount);
-    socket.Close();
+    socket.BeginSend(sendBytes, 0, sendBytes.Length, 0, OnSend, null);
+  }
+
+  private void OnSend(IAsyncResult ar) {
+    try {
+      int bytesCount = socket.EndSend(ar);
+      Debug.Log("Socket send " + bytesCount + " bytes success.");
+    } catch (SocketException ex) {
+      Debug.Log("Socket send failed!" + ex);
+    }
   }
 }
